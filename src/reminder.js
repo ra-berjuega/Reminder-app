a60ff30ef2bdddc7c4181ed0eb4531b8bd75d135
RA <roseannberjuega12@gmail.com>
2023-06-26 06:01:00 +0800

    initial creation

--- src/reminder.js
+++ src/reminder.js
@@ -1,6 +1,26 @@
-export default function Reminder() {
-    return <>
-        <h1>Hello!</h1>
+import { useState } from "react";
 
-    </>
+export default function Reminder() {
+  const [ reminders, setReminder] = useState(["Clean code", "test"])
+  const [ inputList, setInputList] = useState("")
+  const createReminderHtml = () => {
+    return reminders.map((reminder ) =>{
+      return <li>{reminder}</li>   })
+  }
+  const addReminder = () =>{
+    setReminder([...reminders, inputList])
+  }
+   const handleReminderInput = (event) =>{ 
+    setInputList(event.target.value)
+  }
+  
+  return (
+      <>
+          <ul>{createReminderHtml()}</ul>
+
+          <button onClick={addReminder}>Add</button>
+
+          <input onChange={handleReminderInput}></input>
+      </>
+  );
 }
