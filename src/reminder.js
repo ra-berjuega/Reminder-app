34eb76336b7d19be8077ad81df8446801d190d14
RA <roseannberjuega12@gmail.com>
2023-06-28 20:40:33 +0800

    edit remove function

--- src/reminder.js
+++ src/reminder.js
@@ -1,35 +1,45 @@
 import { useState } from 'react';
 
 export default function Reminder() {
-    const [reminders, setReminder] = useState([]);
-    const [inputList, setInputList] = useState('');
-    const createReminderHtml = () =>  {
-        return reminders.map((reminder) => {
-            return (
-                <li key={reminder.id}>
-                    {reminder}
-                    <button onClick={() => removeReminder(reminder.id)}>Remove</button>
-                </li>
-            );
-        });
+  const [reminders, setReminders] = useState([]);
+  const [inputList, setInputList] = useState('');
+
+  const createReminderHtml = () => {
+    return reminders.map((reminder) => {
+      return (
+        <li key={reminder.id}>
+          {reminder.title} {''}
+          <button onClick={() => removeReminder(reminder.id)}>Remove</button>
+        </li>
+      );
+    });
+  };
+
+  const addReminder = () => {
+    const reminder = {
+      id: reminders.length + 1,
+      title: inputList
     };
-    const addReminder = () => {
-        setReminder([...reminders, inputList]);
-    };
-    const handleReminderInput = (event) => {
-        setInputList(event.target.value);
-    };
-    const removeReminder = (id) => {
-        const deleteReminders = reminders.filter((reminder) => reminder.id !== id);
-        setReminder(deleteReminders);
-    };
-    return (
-        <>
-            <div>
-                <ul>{createReminderHtml()}</ul>
-                <button onClick={addReminder}>Add</button>
-                <input onChange={handleReminderInput}></input>
-            </div>
-        </>
-    );
+    setReminders([...reminders, reminder]);
+    setInputList(''); 
+  };
+
+  const handleReminderInput = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const removeReminder = (id) => {
+    const deleteReminders = reminders.filter((reminder) => reminder.id !== id);
+    setReminders(deleteReminders);
+  };
+
+  return (
+    <>
+      <div>
+        <ul>{createReminderHtml()}</ul>
+        <button onClick={addReminder}>Add</button>
+        <input value={inputList} onChange={handleReminderInput}></input>
+      </div>
+    </>
+  );
 }
